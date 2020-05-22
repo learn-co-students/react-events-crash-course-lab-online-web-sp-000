f1d0aef87e2c922e0a08af3eb66cb7d1d828f474
github username <github email address>
2020-05-22 13:56:56 -0600

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -4,19 +4,26 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 
 export default class ChromeBoisDomain extends Component {
   
-  handleMouseMove = (event) => {
+  handleMouseMove = (event) => { //! done?
     /* TODO: This method should capture the `x` and `y` coordinates of the mouse
      * from the event and use them to invoke the `drawChromeBoiAtCoords`
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
+    const xCoordinate = event.screenX;
+    const yCoordinate = event.screenY;
+    drawChromeBoiAtCoords(xCoordinate, yCoordinate);
   }
   
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
-   * listener that should fire it!
+   * listener that should fire it! //! done?
    */
-   
+  handleToggleCycling = () => {
+    toggleCycling();
+  }
+
+
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
   /* is pressed. When a key is pressed, an event handler should invoke the
   /* provided `resize` function with a single argument of either '+' or '-'
@@ -24,10 +31,22 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
   
+  handleKeyPress = (event) => {
+    if (event.charCode === 97){ // if it's 'a'
+      resize('+');
+      console.log('+');
+    } else if (event.charCode === 115) { // if it's 's'
+      resize('-');
+      console.log('-');
+    }
+  }
+
   render() {
     return (
       <canvas 
-        onMouseMove={this.handleMouseMove}
+        onMouseMove={this.handleMouseMove}  //! here when lab started
+        onClick={this.handleToggleCycling}
+        onKeyPress={this.handleKeyPress}
         width='900'
         height='600'
         tabIndex="0">
