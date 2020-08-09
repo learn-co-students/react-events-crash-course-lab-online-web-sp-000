5d318926c4cdc64dda849827a8f5769e262a74e8
Malcolm Leake <echelon1089@gmail.com>
2020-08-09 03:11:00 +0000

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -5,37 +5,27 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 export default class ChromeBoisDomain extends Component {
 
   handleMouseMove = (event) => {
-    /* TODO: This method should capture the `x` and `y` coordinates of the mouse
-     * from the event and use them to invoke the `drawChromeBoiAtCoords`
-     * function that has been provided and is already imported
-     * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
-     */
-
-     drawChromeBoiAtCoords(event.clientX, event.clientY)
+    drawChromeBoiAtCoords(event.clientX, event.clientY);
   }
 
-  handleClick = () => {
-    toggleCycling()
+  handleMouseClick = (event) => {
+    toggleCycling();
   }
 
-  handleKeyPress = (e) => {
-
-  if (e.key === "a")
-  resize('+')
-  else if(e.key === 's'){
-    resize('-')
+  handleKeyPress = (event) => {
+    let legend = {
+      'a': '+',
+      's': '-'
+    }
+    resize(legend[event.key])
   }
-}
-  // - If the key pressed was 'a', then it should invoke `resize` and pass in '+'.
-  // - If the key pressed was 's', then it should invoke `resize` and pass in '-'.
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
    * listener that should fire it!
    */
 
-
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
-  /* is pressed. When a key is pressed, an event handler should invoke the
+  /* is pressed. When a key is pressed, an event handler should invoke the the
   /* provided `resize` function with a single argument of either '+' or '-'
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-'
@@ -45,8 +35,8 @@ export default class ChromeBoisDomain extends Component {
     return (
       <canvas
         onMouseMove={this.handleMouseMove}
-        onClick= {this.handleClick}
-        onKeyPress = {this.handleKeyPress}
+        onClick={this.handleMouseClick}
+        onKeyPress={this.handleKeyPress}
         width='900'
         height='600'
         tabIndex="0">
